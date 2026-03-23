a4225db85dbd1a4d16da143a0998f644ac871ea8
Zeinn95 <unusovisa48@gmail.com>
2026-03-23 20:13:45 +0100

    Version 1

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
-const burger = document.querySelector('.burger');
 const nav = document.querySelector('.nav-container');
+const burger = document.querySelector('.burger');
+const close = document.querySelector('.close');
 
+// Функция для открытия
 burger.addEventListener('click', () => {
-  nav.classList.toggle('nav-active');
+    nav.classList.add('nav-active');
+    burger.classList.add('nav-active');
+    close.classList.add('nav-active');
+});
+
+// Функция для закрытия (нажимаем на крестик)
+close.addEventListener('click', () => {
+    nav.classList.remove('nav-active');
+    burger.classList.remove('nav-active');
+    close.classList.remove('nav-active');
 });
