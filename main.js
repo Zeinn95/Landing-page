c9de614c6bfab152e1d5a02dc18ca7cadea7aecc
Zeinn95 <unusovisa48@gmail.com>
2026-03-24 01:09:05 +0100

    Сделал welcome-block

--- main.js
+++ main.js
@@ -2,14 +2,12 @@ const nav = document.querySelector('.nav-container');
 const burger = document.querySelector('.burger');
 const close = document.querySelector('.close');
 
-// Функция для открытия
 burger.addEventListener('click', () => {
     nav.classList.add('nav-active');
     burger.classList.add('nav-active');
     close.classList.add('nav-active');
 });
 
-// Функция для закрытия (нажимаем на крестик)
 close.addEventListener('click', () => {
     nav.classList.remove('nav-active');
     burger.classList.remove('nav-active');
