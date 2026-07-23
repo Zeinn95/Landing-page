e9dceaabebccd957cb6321ea9859f413c5d72d9e
Zeinn95 <unusovisa48@gmail.com>
2026-07-23 15:25:17 +0200

    light changes

--- main.js
+++ main.js
@@ -19,3 +19,24 @@ navLinks.querySelectorAll('a').forEach(link => {
         menuContainer.classList.remove('show-close');
     });
 });
+
+const swiper = new Swiper('.swiper', {  
+  // 1. Включаем центрирование активного слайда
+  centeredSlides: true, 
+  
+  // 2. Указываем дробное число. 
+  // Например, 1.2 означает: 1 целый слайд в центре + по 0.1 (10%) от соседних по бокам
+  slidesPerView: 1.2, 
+  
+  spaceBetween: 20, 
+
+  // 3. Настраиваем адаптивность: на больших экранах показываем больше кусочков
+  breakpoints: {
+    640: {
+      slidesPerView: 1.5, // 1 по центру, по 25% по бокам
+    },
+    1024: {
+      slidesPerView: 2.5, // На ПК можно показать больше
+    }
+  }
+});
